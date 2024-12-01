1a1a5262cc420bb63b6f58d91fba8d6db8bcb42a
zeshalondrag <144255214+zeshalondrag@users.noreply.github.com>
2024-12-01 21:31:08 +0300

    PCstore.sql

--- PCstore.sql
+++ PCstore.sql
@@ -15,9 +15,9 @@ GO
 
 INSERT INTO Users (UserSurname, UserName, UserMiddleName, Phone, Logins, Passwords)
 VALUES
-	('Сизова','Варвара','Робертовна','+79585954810','sizova@gmail.com','sizova123'),
-	('Симонов','Михаил','Дмитриевич','+79370883342','simonov@gmail.com','simonov123'),
-	('Новиков','Сергей','Михайлович','+79955248160','novikov@gmail.com','novikov123')
+	('РЎРёР·РѕРІР°','Р’Р°СЂРІР°СЂР°','Р РѕР±РµСЂС‚РѕРІРЅР°','+79585954810','sizova@gmail.com','sizova123'),
+	('РЎРёРјРѕРЅРѕРІ','РњРёС…Р°РёР»','Р”РјРёС‚СЂРёРµРІРёС‡','+79370883342','simonov@gmail.com','simonov123'),
+	('РќРѕРІРёРєРѕРІ','РЎРµСЂРіРµР№','РњРёС…Р°Р№Р»РѕРІРёС‡','+79955248160','novikov@gmail.com','novikov123')
 	
 SELECT * FROM Users;
 
@@ -29,9 +29,9 @@ GO
 
 INSERT INTO Categorys(Names)
 VALUES
-	('Игровые компьютеры'),
-	('Рабочие станции'),
-	('Серверы')
+	('РРіСЂРѕРІС‹Рµ РєРѕРјРїСЊСЋС‚РµСЂС‹'),
+	('Р Р°Р±РѕС‡РёРµ СЃС‚Р°РЅС†РёРё'),
+	('РЎРµСЂРІРµСЂС‹')
 
 CREATE TABLE Catalogs (
     ID_Catalog INT PRIMARY KEY IDENTITY(1,1),
@@ -46,15 +46,15 @@ GO
 
 INSERT INTO Catalogs(Names, Descriptions, Price, Quantity, Category_ID, Image_url)
 VALUES
-	('HYPERPC FURY', 'Геймерская система на базе Intel® Core™ i5-12400F и видеокарты MSI GeForce RTX 4060 VENTUS 2X [8GB, 3072 CUDA].', 118100.0, 10, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_fury_4355/hyperpc-fury-black-v3-upd-450x450.jpg'),
-	('HYPERPC WARRIOR', 'Производительный компьютер для игр и работы на базе Intel® Core™ i5-13400F и видеокарты MSI GeForce RTX 4060 Ti [8GB, 4352 CUDA].', 158700.0, 4, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_warrior_4626/hyperpc-warrior-white-alt-450x450.jpg'),
-	('HYPERPC CHAMPION', 'Мощная система на базе Intel® Core™ i5-14400F и видеокарты Palit GeForce RTX 4070 JetStream [12GB, 5888 CUDA].', 204800.0, 1, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_champion_4356/hyperpc-champion-d41-screen-450x450.jpg'),
-	('HYPERPC G5 PRO', 'Мощная рабочая станция для работы с видеокартой MSI GeForce RTX 4090 [24GB, 16384 CUDA] и процессором Intel® Core™ i9-14900K [до 6.0GHz, 24 ядер].', 578800.0, 2, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g5_pro_4564/hyperpc-g5-pro-alt-450x450.jpg'),
-	('HYPERPC G6 PRO', 'Мощная рабочая станция для задач любой сложности. В основе: NVIDIA RTX A6000 [48GB, 10752 CUDA] и процессор Intel® Core™ i9-14900K [до 6.0GHz, 24 ядер].', 1074400.0, 8, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g6_pro_4565/hyperpc-g6-black-alt-upd-450x450.jpg'),
-	('HYPERPC G7 PRO', 'Высокопроизводительная станция для создания и обработки 3D-графики с видеокартой NVIDIA RTX 6000 Ada Generation [48GB, 18176 CUDA] и процессором AMD Ryzen Threadripper PRO 7956WX [до 5.3GHz, 24 ядер].', 2297900.0, 6, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g7_pro_4566/hyperpc-g7-pro-upd-450x450.jpg'),
-	('FORCE CORE', 'Серверы для основных IT-задач, таких как управление веб-сервисами, базами данных и файловыми хранилищами. FORCE CORE обеспечивает стабильную и надежную работу для малых и средних бизнесов с возможностью масштабирования по мере роста. До 2x процессоров, до 256 ядер (Intel Xeon 2nd-4th Gen, AMD EPYC) До 4TB памяти DDR4/DDR5 ECC Контроллеры до 16 жестких дисков (SAS, NVMe)', 0.0, 1, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-1-unit-blk.jpg?1'),
-	('FORCE POWER', 'Мощные серверы, предназначенные для более сложных задач, таких как виртуализация, аналитика и работа с большими массивами данных. FORCE POWER предлагает идеальное сочетание производительности и гибкости для построения надежной инфраструктуры.  До 4x процессоров, до 256 ядер (Intel Xeon 2nd-5th Gen, AMD EPYC) До 8TB памяти DDR4/DDR5 ECC Контроллеры до 24 жестких дисков (SAS, NVMe)', 0.0, 1, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-2-unit-blk.jpg?1'),
-	('FORCE ULTRA', 'Сервера премиум-класса для критически важных бизнес-систем. FORCE ULTRA предлагает максимальную производительность, надежность системы и масштабируемость, что делает их идеальным выбором для организаций с высокими требованиями к надежности и безопасности. До 8x процессоров, до 256 ядер (Intel Xeon 2nd-5th Gen, AMD EPYC) До 16TB памяти DDR4/DDR5 ECC Контроллеры до 48 жестких дисков (SAS, NVMe)', 0.0, 2, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-4-unit-blk.jpg?1')
+	('HYPERPC FURY', 'Р“РµР№РјРµСЂСЃРєР°СЏ СЃРёСЃС‚РµРјР° РЅР° Р±Р°Р·Рµ IntelВ® Coreв„ў i5-12400F Рё РІРёРґРµРѕРєР°СЂС‚С‹ MSI GeForce RTX 4060 VENTUS 2X [8GB, 3072 CUDA].', 118100.0, 10, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_fury_4355/hyperpc-fury-black-v3-upd-450x450.jpg'),
+	('HYPERPC WARRIOR', 'РџСЂРѕРёР·РІРѕРґРёС‚РµР»СЊРЅС‹Р№ РєРѕРјРїСЊСЋС‚РµСЂ РґР»СЏ РёРіСЂ Рё СЂР°Р±РѕС‚С‹ РЅР° Р±Р°Р·Рµ IntelВ® Coreв„ў i5-13400F Рё РІРёРґРµРѕРєР°СЂС‚С‹ MSI GeForce RTX 4060 Ti [8GB, 4352 CUDA].', 158700.0, 4, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_warrior_4626/hyperpc-warrior-white-alt-450x450.jpg'),
+	('HYPERPC CHAMPION', 'РњРѕС‰РЅР°СЏ СЃРёСЃС‚РµРјР° РЅР° Р±Р°Р·Рµ IntelВ® Coreв„ў i5-14400F Рё РІРёРґРµРѕРєР°СЂС‚С‹ Palit GeForce RTX 4070 JetStream [12GB, 5888 CUDA].', 204800.0, 1, 1, 'https://hyperpc.ru/cache/hp_position_hyperpc_champion_4356/hyperpc-champion-d41-screen-450x450.jpg'),
+	('HYPERPC G5 PRO', 'РњРѕС‰РЅР°СЏ СЂР°Р±РѕС‡Р°СЏ СЃС‚Р°РЅС†РёСЏ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ РІРёРґРµРѕРєР°СЂС‚РѕР№ MSI GeForce RTX 4090 [24GB, 16384 CUDA] Рё РїСЂРѕС†РµСЃСЃРѕСЂРѕРј IntelВ® Coreв„ў i9-14900K [РґРѕ 6.0GHz, 24 СЏРґРµСЂ].', 578800.0, 2, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g5_pro_4564/hyperpc-g5-pro-alt-450x450.jpg'),
+	('HYPERPC G6 PRO', 'РњРѕС‰РЅР°СЏ СЂР°Р±РѕС‡Р°СЏ СЃС‚Р°РЅС†РёСЏ РґР»СЏ Р·Р°РґР°С‡ Р»СЋР±РѕР№ СЃР»РѕР¶РЅРѕСЃС‚Рё. Р’ РѕСЃРЅРѕРІРµ: NVIDIA RTX A6000 [48GB, 10752 CUDA] Рё РїСЂРѕС†РµСЃСЃРѕСЂ IntelВ® Coreв„ў i9-14900K [РґРѕ 6.0GHz, 24 СЏРґРµСЂ].', 1074400.0, 8, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g6_pro_4565/hyperpc-g6-black-alt-upd-450x450.jpg'),
+	('HYPERPC G7 PRO', 'Р’С‹СЃРѕРєРѕРїСЂРѕРёР·РІРѕРґРёС‚РµР»СЊРЅР°СЏ СЃС‚Р°РЅС†РёСЏ РґР»СЏ СЃРѕР·РґР°РЅРёСЏ Рё РѕР±СЂР°Р±РѕС‚РєРё 3D-РіСЂР°С„РёРєРё СЃ РІРёРґРµРѕРєР°СЂС‚РѕР№ NVIDIA RTX 6000 Ada Generation [48GB, 18176 CUDA] Рё РїСЂРѕС†РµСЃСЃРѕСЂРѕРј AMD Ryzen Threadripper PRO 7956WX [РґРѕ 5.3GHz, 24 СЏРґРµСЂ].', 2297900.0, 6, 2, 'https://hyperpc.ru/cache/hp_position_hyperpc_g7_pro_4566/hyperpc-g7-pro-upd-450x450.jpg'),
+	('FORCE CORE', 'РЎРµСЂРІРµСЂС‹ РґР»СЏ РѕСЃРЅРѕРІРЅС‹С… IT-Р·Р°РґР°С‡, С‚Р°РєРёС… РєР°Рє СѓРїСЂР°РІР»РµРЅРёРµ РІРµР±-СЃРµСЂРІРёСЃР°РјРё, Р±Р°Р·Р°РјРё РґР°РЅРЅС‹С… Рё С„Р°Р№Р»РѕРІС‹РјРё С…СЂР°РЅРёР»РёС‰Р°РјРё. FORCE CORE РѕР±РµСЃРїРµС‡РёРІР°РµС‚ СЃС‚Р°Р±РёР»СЊРЅСѓСЋ Рё РЅР°РґРµР¶РЅСѓСЋ СЂР°Р±РѕС‚Сѓ РґР»СЏ РјР°Р»С‹С… Рё СЃСЂРµРґРЅРёС… Р±РёР·РЅРµСЃРѕРІ СЃ РІРѕР·РјРѕР¶РЅРѕСЃС‚СЊСЋ РјР°СЃС€С‚Р°Р±РёСЂРѕРІР°РЅРёСЏ РїРѕ РјРµСЂРµ СЂРѕСЃС‚Р°. Р”Рѕ 2x РїСЂРѕС†РµСЃСЃРѕСЂРѕРІ, РґРѕ 256 СЏРґРµСЂ (Intel Xeon 2nd-4th Gen, AMD EPYC) Р”Рѕ 4TB РїР°РјСЏС‚Рё DDR4/DDR5 ECC РљРѕРЅС‚СЂРѕР»Р»РµСЂС‹ РґРѕ 16 Р¶РµСЃС‚РєРёС… РґРёСЃРєРѕРІ (SAS, NVMe)', 0.0, 1, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-1-unit-blk.jpg?1'),
+	('FORCE POWER', 'РњРѕС‰РЅС‹Рµ СЃРµСЂРІРµСЂС‹, РїСЂРµРґРЅР°Р·РЅР°С‡РµРЅРЅС‹Рµ РґР»СЏ Р±РѕР»РµРµ СЃР»РѕР¶РЅС‹С… Р·Р°РґР°С‡, С‚Р°РєРёС… РєР°Рє РІРёСЂС‚СѓР°Р»РёР·Р°С†РёСЏ, Р°РЅР°Р»РёС‚РёРєР° Рё СЂР°Р±РѕС‚Р° СЃ Р±РѕР»СЊС€РёРјРё РјР°СЃСЃРёРІР°РјРё РґР°РЅРЅС‹С…. FORCE POWER РїСЂРµРґР»Р°РіР°РµС‚ РёРґРµР°Р»СЊРЅРѕРµ СЃРѕС‡РµС‚Р°РЅРёРµ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЊРЅРѕСЃС‚Рё Рё РіРёР±РєРѕСЃС‚Рё РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ РЅР°РґРµР¶РЅРѕР№ РёРЅС„СЂР°СЃС‚СЂСѓРєС‚СѓСЂС‹.  Р”Рѕ 4x РїСЂРѕС†РµСЃСЃРѕСЂРѕРІ, РґРѕ 256 СЏРґРµСЂ (Intel Xeon 2nd-5th Gen, AMD EPYC) Р”Рѕ 8TB РїР°РјСЏС‚Рё DDR4/DDR5 ECC РљРѕРЅС‚СЂРѕР»Р»РµСЂС‹ РґРѕ 24 Р¶РµСЃС‚РєРёС… РґРёСЃРєРѕРІ (SAS, NVMe)', 0.0, 1, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-2-unit-blk.jpg?1'),
+	('FORCE ULTRA', 'РЎРµСЂРІРµСЂР° РїСЂРµРјРёСѓРј-РєР»Р°СЃСЃР° РґР»СЏ РєСЂРёС‚РёС‡РµСЃРєРё РІР°Р¶РЅС‹С… Р±РёР·РЅРµСЃ-СЃРёСЃС‚РµРј. FORCE ULTRA РїСЂРµРґР»Р°РіР°РµС‚ РјР°РєСЃРёРјР°Р»СЊРЅСѓСЋ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЊРЅРѕСЃС‚СЊ, РЅР°РґРµР¶РЅРѕСЃС‚СЊ СЃРёСЃС‚РµРјС‹ Рё РјР°СЃС€С‚Р°Р±РёСЂСѓРµРјРѕСЃС‚СЊ, С‡С‚Рѕ РґРµР»Р°РµС‚ РёС… РёРґРµР°Р»СЊРЅС‹Рј РІС‹Р±РѕСЂРѕРј РґР»СЏ РѕСЂРіР°РЅРёР·Р°С†РёР№ СЃ РІС‹СЃРѕРєРёРјРё С‚СЂРµР±РѕРІР°РЅРёСЏРјРё Рє РЅР°РґРµР¶РЅРѕСЃС‚Рё Рё Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё. Р”Рѕ 8x РїСЂРѕС†РµСЃСЃРѕСЂРѕРІ, РґРѕ 256 СЏРґРµСЂ (Intel Xeon 2nd-5th Gen, AMD EPYC) Р”Рѕ 16TB РїР°РјСЏС‚Рё DDR4/DDR5 ECC РљРѕРЅС‚СЂРѕР»Р»РµСЂС‹ РґРѕ 48 Р¶РµСЃС‚РєРёС… РґРёСЃРєРѕРІ (SAS, NVMe)', 0.0, 2, 3, 'https://hyperpc.ru/images/product/servers/main/force/hyperpc-force-4-unit-blk.jpg?1')
 
 SELECT * FROM Catalogs;
 GO
